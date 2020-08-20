9e69eba7f6374b95bd15ca8d8a05f6f79e3f25f0
waqarkhan6 <vakarkhan8686@yahoo.com>
2020-08-19 21:30:25 -0400

    my first commit

---
+++ src/test/java/com/cbt/utilities/BrowserFactory.java
@@ -0,0 +1,11 @@
+package com.cbt.utilities;
+
+import org.openqa.selenium.WebDriver;
+
+public class BrowserFactory {
+
+    public static WebDriver getDriver(String browser){
+        return null;
+
+    }
+}
